70b2b1b55377b457be693fd4a19d23910eebe74a
Lucas <lucascamargo.dev@gmail.com>
2021-11-16 09:29:23 -0300

    Enviando Pag DuvidaItems

---
+++ resources/js/lib/listDuvidas.js
@@ -0,0 +1,22 @@
+export const list = [
+    {
+        title: "Titulo1",
+        description: "Description1",
+    },
+    {
+        title: "Titulo2",
+        description: "Description2",
+    },
+    {
+        title: "Titulo3",
+        description: "Description3",
+    },
+    {
+        title: "Titulo4",
+        description: "Description4",
+    },
+    {
+        title: "Titulo5",
+        description: "Description5",
+    },
+];
