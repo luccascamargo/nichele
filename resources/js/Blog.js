205e5c3359a4cd0cb1f24c0f20123c42a4dd94bd
MacawBrasil <37770361+AllanKDeveloper@users.noreply.github.com>
2022-02-10 08:57:27 -0300

    Revisao marcus v2

--- resources/js/Blog.js
+++ resources/js/Blog.js
@@ -1,46 +1,50 @@
 /* eslint-disable no-irregular-whitespace */
 /* eslint-disable import/no-unresolved */
 import ReactDOM from "react-dom";
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
 import { Navbar } from "./components/Navbar";
 import { Footer } from "./components/Footer";
-
 
 import "../sass/blog.scss";
 
 export default function Blog() {
     const [cmsInfo, setCmsInfo] = useState({});
-    const [posts, setPosts] = useState([])
+    const [posts, setPosts] = useState([]);
 
     useEffect(() => {
         const getCmsInfo = async () => {
-            await fetch('https://fathomless-chamber-79732.herokuapp.com/api/info', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            .then(response => response.json())
-            .then(data => setCmsInfo(data.data.attributes));
-        }
+            await fetch(
+                "https://fathomless-chamber-79732.herokuapp.com/api/info",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((response) => response.json())
+                .then((data) => setCmsInfo(data.data.attributes));
+        };
         getCmsInfo();
-    }, [])
+    }, []);
 
     useEffect(() => {
         const getBlog = async () => {
-            await fetch('https://fathomless-chamber-79732.herokuapp.com/api/blogs?populate=*', {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-            })
-            .then(response => response.json())
-            .then(data => setPosts(data.data));
-        }
+            await fetch(
+                "https://fathomless-chamber-79732.herokuapp.com/api/blogs?populate=*",
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }
+            )
+                .then((response) => response.json())
+                .then((data) => setPosts(data.data));
+        };
         getBlog();
-    }, [])
-
+    }, []);
 
     return (
         <>
@@ -53,31 +57,42 @@ export default function Blog() {
 
             <div className="blog">
                 <div className="container">
-                    {posts?.map(item => {
+                    {posts?.map((item) => {
                         const imageBlog = `${
-                            item.attributes.image.data.attributes.url.startsWith(`/`)
-                              ? "https://fathomless-chamber-79732.herokuapp.com/"
-                              : ``
-                          }${item.attributes.image.data.attributes.url}`;
-                          console.log(item)
+                            item.attributes.image.data.attributes.url.startsWith(
+                                `/`
+                            )
+                                ? "https://fathomless-chamber-79732.herokuapp.com/"
+                                : ``
+                        }${item.attributes.image.data.attributes.url}`;
+
+                        let date = new Date(item.attributes.createdAt);
+                        date = date.toLocaleString();
                         return (
-                            <a key={item.id} href={`/blogcontent?code=${item.id}`} className="item">
-                            <img src={imageBlog} alt={item.attributes.title} />
-                            <div className="info">
-                                <span className="date">{item.attributes.createdAt}</span>
-                                <h2>{item.attributes.title}</h2>
-                                <a className="more" href={`/blogcontent?code=${item.id}`}>
-                                    Leia mais
-                                </a>
-                            </div>
-                        </a>
-                        )
+                            <a
+                                key={item.id}
+                                href={`/blogcontent?code=${item.id}`}
+                                className="item"
+                            >
+                                <img
+                                    src={imageBlog}
+                                    alt={item.attributes.title}
+                                />
+                                <div className="info">
+                                    <span className="date">{date}</span>
+                                    <h2>{item.attributes.title}</h2>
+                                    <span className="more">Leia mais</span>
+                                </div>
+                            </a>
+                        );
                     })}
                 </div>
             </div>
 
-            <Footer data={cmsInfo}/>
-        </>)}
+            <Footer data={cmsInfo} />
+        </>
+    );
+}
 
 if (document.getElementById("blog")) {
     ReactDOM.render(<Blog />, document.getElementById("blog"));
